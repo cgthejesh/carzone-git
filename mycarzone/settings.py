abd3889c0f9f0694ab170860a40d01c7c8176028
cgthejesh <thejesh.cg@gmail.com>
2021-01-18 19:40:02 +0530

    debug set to True

--- mycarzone/settings.py
+++ mycarzone/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ckeditor',
-    'django.contrib.humanize'
+    'django.contrib.humanize',
+    'mycarzone',
 ]
 
 MIDDLEWARE = [
